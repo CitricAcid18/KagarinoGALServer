5c1733bca7b06a02edc8deb9e35889451242fae7
wenjie <x3011703521@163.com>
2024-06-07 11:24:44 +0800

    异常处理，结果类

---
+++ src/main/java/com/kagarino/webserver/Until/ResultEnum.java
@@ -0,0 +1,38 @@
+package com.kagarino.webserver.Until;
+
+/**
+ * @Author: zwj
+ * @Date: 2024/6/7 9:43
+ * @Version: v1.0.0
+ * @Description: TODO 结果枚举类
+ **/
+public enum ResultEnum {
+
+    SUCCESS("200", "操作成功"),
+    CREATED("201", "对象创建成功"),
+    ACCEPTED("202", "请求已经被接受"),
+    NO_CONTENT("204", "操作已经执行成功，但是没有返回数据"),
+    MOVED_PERM("301", "资源已被移除"),
+    SEE_OTHER("303", "重定向"),
+    NOT_MODIFIED("304", "资源没有被修改"),
+    BAD_REQUEST("400", "参数列表错误（缺少，格式不匹配）"),
+    UNAUTHORIZED("401", "未授权"),
+    FORBIDDEN("403", "访问受限，授权过期"),
+    NOT_FOUND("404", "资源，服务未找到"),
+    BAD_METHOD("405", "不允许的http方法"),
+    CONFLICT("409", "资源冲突，或者资源被锁"),
+    UNSUPPORTED_TYPE("415", "不支持的数据，媒体类型"),
+    ERROR("500", "系统内部错误"),
+    NOT_IMPLEMENTED("501", "接口未实现"),
+    BAD_SQL("502", "sql异常"),
+            ;
+
+    public String code;
+    public String msg;
+
+    ResultEnum(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+}
---
+++ src/main/java/com/kagarino/webserver/Until/ServiceException.java
@@ -0,0 +1,74 @@
+package com.kagarino.webserver.Until;
+
+/**
+ * @Author: zwj
+ * @Date: 2024/6/7 10:13
+ * @Version: v1.0.0
+ * @Description: TODO 业务异常封装类
+ **/
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 错误码
+     */
+    private String code;
+
+    /**
+     * 错误提示
+     */
+    private String message;
+
+    /**
+     * 错误明细，内部调试错误
+     */
+    private String detailMessage;
+
+    /**
+     * 空构造方法，避免反序列化问题
+     */
+    public ServiceException()
+    {
+    }
+
+    public ServiceException(String message)
+    {
+        this.message = message;
+    }
+
+    public ServiceException(String message, String code)
+    {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getDetailMessage()
+    {
+        return detailMessage;
+    }
+
+    @Override
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public ServiceException setMessage(String message)
+    {
+        this.message = message;
+        return this;
+    }
+
+    public ServiceException setDetailMessage(String detailMessage)
+    {
+        this.detailMessage = detailMessage;
+        return this;
+    }
+
+}
