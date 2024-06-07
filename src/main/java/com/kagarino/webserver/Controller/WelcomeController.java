991ce58f73976f24644d8800462cc5832be8069a
wenjie <x3011703521@163.com>
2024-06-07 21:52:11 +0800

    注册

--- src/main/java/com/kagarino/webserver/Controller/WelcomeController.java
+++ src/main/java/com/kagarino/webserver/Controller/WelcomeController.java
@@ -1,6 +1,11 @@
 package com.kagarino.webserver.Controller;
 
 import com.kagarino.webserver.Entity.Result;
+import com.kagarino.webserver.Mapper.KagarinoUserMapper;
+import com.kagarino.webserver.Service.KagarinoUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: TODO 实现欢迎页相关功能的控制类
  **/
 @RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
+
+    @Autowired
+    private KagarinoUserService kagarinoUserService;
 
     /**
      * @Auther: zwj
@@ -19,7 +28,10 @@ public class WelcomeController {
      * @Params: 用户名username 用户密码password 用户邮箱mail
      * @Return: 成功与否及提示
      */
-    public Result<String> createUser(){
+    @PostMapping("/user")
+    public Result<String> createUser(String username,String password,String mail){
+        //验证该用户名是否唯一
+//        kagarinoUserMapper.
         return null;
     }
 
--- src/main/java/com/kagarino/webserver/Mapper/KagarinoUserMapper.java
+++ src/main/java/com/kagarino/webserver/Mapper/KagarinoUserMapper.java
@@ -14,5 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface KagarinoUserMapper extends BaseMapper<KagarinoUser> {
+    public Boolean isUsernameExist(String username);
 
+    public Boolean isMailExist(String mail);
 }
